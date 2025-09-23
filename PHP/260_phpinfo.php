0445ef43766814255aa43cf66a2498208fa5ed3a
Daniel Dmitrovski <daniel.dmitrovski@icloud.com>
2025-09-23 11:40:27 +0200

    Add files via upload

---
+++ PHP/260_phpinfo.php
@@ -0,0 +1,18 @@
+<?php
+
+phpinfo();
+
+// Apache-Webserver Konfigurationsdatei:
+//C:\xampp\apache\conf\httpd.conf
+
+// PHP-Konfigurationsdatei
+//C:\xampp\php\php.ini
+
+// Mysql-Konfigurationsdatei
+//C:\xampp\mysql\bin\my.ini
+
+// Um Debugger in PHP zu aktivieren,
+// muss man die PHP-Erweiterung XDEBUG einbinden.
+// Das geht am besten heutzutage direkt in der IDE.
+
+//https://xdebug.org/wizard
